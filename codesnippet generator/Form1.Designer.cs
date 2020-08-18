45646028bd7abbcfc60941a1cfe8fe15b05ebd0d
Gonzalo santiago martinez <simpleverso@users.noreply.github.com>
2020-08-18 16:21:22 -0500

    Completo
    Completo

--- codesnippet generator/Form1.Designer.cs
+++ codesnippet generator/Form1.Designer.cs
@@ -50,11 +50,16 @@
             this.label9 = new System.Windows.Forms.Label();
             this.txt_usings = new System.Windows.Forms.TextBox();
             this.label10 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.txt_direccion = new System.Windows.Forms.TextBox();
+            this.check_final = new System.Windows.Forms.CheckBox();
+            this.label12 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // txt_Codigo
             // 
-            this.txt_Codigo.Location = new System.Drawing.Point(15, 127);
+            this.txt_Codigo.Location = new System.Drawing.Point(19, 195);
             this.txt_Codigo.Multiline = true;
             this.txt_Codigo.Name = "txt_Codigo";
             this.txt_Codigo.Size = new System.Drawing.Size(394, 196);
@@ -63,7 +68,7 @@
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(24, 9);
+            this.label1.Location = new System.Drawing.Point(28, 77);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(358, 13);
             this.label1.TabIndex = 1;
@@ -72,7 +77,7 @@
             // button1
             // 
             this.button1.DialogResult = System.Windows.Forms.DialogResult.Cancel;
-            this.button1.Location = new System.Drawing.Point(12, 344);
+            this.button1.Location = new System.Drawing.Point(16, 412);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(137, 67);
             this.button1.TabIndex = 2;
@@ -83,7 +88,7 @@
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(12, 111);
+            this.label2.Location = new System.Drawing.Point(16, 179);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(145, 13);
             this.label2.TabIndex = 3;
@@ -91,7 +96,7 @@
             // 
             // txt_ID
             // 
-            this.txt_ID.Location = new System.Drawing.Point(422, 78);
+            this.txt_ID.Location = new System.Drawing.Point(429, 188);
             this.txt_ID.Multiline = true;
             this.txt_ID.Name = "txt_ID";
             this.txt_ID.Size = new System.Drawing.Size(243, 32);
@@ -100,7 +105,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(422, 62);
+            this.label3.Location = new System.Drawing.Point(429, 172);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(18, 13);
             this.label3.TabIndex = 5;
@@ -109,7 +114,7 @@
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(422, 127);
+            this.label4.Location = new System.Drawing.Point(429, 237);
             this.label4.Name = "label4";
             this.label4.Size = new System.Drawing.Size(63, 13);
             this.label4.TabIndex = 6;
@@ -117,7 +122,7 @@
             // 
             // Txt_descripcion
             // 
-            this.Txt_descripcion.Location = new System.Drawing.Point(422, 143);
+            this.Txt_descripcion.Location = new System.Drawing.Point(429, 253);
             this.Txt_descripcion.Multiline = true;
             this.Txt_descripcion.Name = "Txt_descripcion";
             this.Txt_descripcion.Size = new System.Drawing.Size(243, 78);
@@ -125,7 +130,7 @@
             // 
             // Txt_predeterminado
             // 
-            this.Txt_predeterminado.Location = new System.Drawing.Point(422, 247);
+            this.Txt_predeterminado.Location = new System.Drawing.Point(429, 357);
             this.Txt_predeterminado.Multiline = true;
             this.Txt_predeterminado.Name = "Txt_predeterminado";
             this.Txt_predeterminado.Size = new System.Drawing.Size(243, 53);
@@ -134,7 +139,7 @@
             // label5
             // 
             this.label5.AutoSize = true;
-            this.label5.Location = new System.Drawing.Point(422, 231);
+            this.label5.Location = new System.Drawing.Point(429, 341);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(81, 13);
             this.label5.TabIndex = 8;
@@ -143,7 +148,7 @@
             // textBox5
             // 
             this.textBox5.Enabled = false;
-            this.textBox5.Location = new System.Drawing.Point(27, 38);
+            this.textBox5.Location = new System.Drawing.Point(31, 106);
             this.textBox5.Multiline = true;
             this.textBox5.Name = "textBox5";
             this.textBox5.ReadOnly = true;
@@ -153,14 +158,14 @@
             // 
             // txt_Titulo
             // 
-            this.txt_Titulo.Location = new System.Drawing.Point(211, 329);
+            this.txt_Titulo.Location = new System.Drawing.Point(247, 397);
             this.txt_Titulo.Name = "txt_Titulo";
             this.txt_Titulo.Size = new System.Drawing.Size(151, 20);
             this.txt_Titulo.TabIndex = 11;
             // 
             // Txt_accesodirecto
             // 
-            this.Txt_accesodirecto.Location = new System.Drawing.Point(262, 355);
+            this.Txt_accesodirecto.Location = new System.Drawing.Point(298, 423);
             this.Txt_accesodirecto.Name = "Txt_accesodirecto";
             this.Txt_accesodirecto.Size = new System.Drawing.Size(100, 20);
             this.Txt_accesodirecto.TabIndex = 12;
@@ -168,7 +173,7 @@
             // label6
             // 
             this.label6.AutoSize = true;
-            this.label6.Location = new System.Drawing.Point(170, 332);
+            this.label6.Location = new System.Drawing.Point(206, 400);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(33, 13);
             this.label6.TabIndex = 13;
@@ -177,16 +182,16 @@
             // label7
             // 
             this.label7.AutoSize = true;
-            this.label7.Location = new System.Drawing.Point(170, 358);
+            this.label7.Location = new System.Drawing.Point(206, 426);
             this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(78, 13);
+            this.label7.Size = new System.Drawing.Size(86, 13);
             this.label7.TabIndex = 14;
-            this.label7.Text = "Acceso directo";
+            this.label7.Text = "Acceso directo**";
             // 
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Location = new System.Drawing.Point(155, 398);
+            this.label8.Location = new System.Drawing.Point(191, 466);
             this.label8.Name = "label8";
             this.label8.Size = new System.Drawing.Size(60, 13);
             this.label8.TabIndex = 16;
@@ -194,7 +199,7 @@
             // 
             // Txt_comentario
             // 
-            this.Txt_comentario.Location = new System.Drawing.Point(211, 381);
+            this.Txt_comentario.Location = new System.Drawing.Point(247, 449);
             this.Txt_comentario.Multiline = true;
             this.Txt_comentario.Name = "Txt_comentario";
             this.Txt_comentario.Size = new System.Drawing.Size(151, 52);
@@ -215,7 +220,7 @@
             "cpp",
             "XAML",
             "XML"});
-            this.cmb_lenguajes.Location = new System.Drawing.Point(422, 38);
+            this.cmb_lenguajes.Location = new System.Drawing.Point(426, 106);
             this.cmb_lenguajes.Name = "cmb_lenguajes";
             this.cmb_lenguajes.Size = new System.Drawing.Size(241, 21);
             this.cmb_lenguajes.TabIndex = 18;
@@ -223,7 +228,7 @@
             // label9
             // 
             this.label9.AutoSize = true;
-            this.label9.Location = new System.Drawing.Point(422, 19);
+            this.label9.Location = new System.Drawing.Point(426, 87);
             this.label9.Name = "label9";
             this.label9.Size = new System.Drawing.Size(51, 13);
             this.label9.TabIndex = 19;
@@ -231,7 +236,7 @@
             // 
             // txt_usings
             // 
-            this.txt_usings.Location = new System.Drawing.Point(422, 329);
+            this.txt_usings.Location = new System.Drawing.Point(429, 439);
             this.txt_usings.Multiline = true;
             this.txt_usings.Name = "txt_usings";
             this.txt_usings.Size = new System.Drawing.Size(243, 74);
@@ -240,17 +245,68 @@
             // label10
             // 
             this.label10.AutoSize = true;
-            this.label10.Location = new System.Drawing.Point(422, 313);
+            this.label10.Location = new System.Drawing.Point(429, 423);
             this.label10.Name = "label10";
             this.label10.Size = new System.Drawing.Size(34, 13);
             this.label10.TabIndex = 20;
             this.label10.Text = "Using";
             // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Location = new System.Drawing.Point(31, 29);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(52, 13);
+            this.label11.TabIndex = 22;
+            this.label11.Text = "Direccion";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(536, 12);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(133, 47);
+            this.button2.TabIndex = 23;
+            this.button2.Text = "Abrir Snippet";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // txt_direccion
+            // 
+            this.txt_direccion.Enabled = false;
+            this.txt_direccion.Location = new System.Drawing.Point(93, 25);
+            this.txt_direccion.Name = "txt_direccion";
+            this.txt_direccion.Size = new System.Drawing.Size(426, 20);
+            this.txt_direccion.TabIndex = 24;
+            // 
+            // check_final
+            // 
+            this.check_final.AutoSize = true;
+            this.check_final.Location = new System.Drawing.Point(429, 139);
+            this.check_final.Name = "check_final";
+            this.check_final.Size = new System.Drawing.Size(124, 17);
+            this.check_final.TabIndex = 25;
+            this.check_final.Text = "Final predeterminado";
+            this.check_final.UseVisualStyleBackColor = true;
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Location = new System.Drawing.Point(16, 519);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(146, 13);
+            this.label12.TabIndex = 26;
+            this.label12.Text = "**Se guarda con este nombre";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(681, 445);
+            this.ClientSize = new System.Drawing.Size(718, 548);
+            this.Controls.Add(this.label12);
+            this.Controls.Add(this.check_final);
+            this.Controls.Add(this.txt_direccion);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label11);
             this.Controls.Add(this.txt_usings);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
@@ -304,6 +360,11 @@
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.TextBox txt_usings;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.TextBox txt_direccion;
+        private System.Windows.Forms.CheckBox check_final;
+        private System.Windows.Forms.Label label12;
     }
 }
 
--- codesnippet generator/Form1.cs
+++ codesnippet generator/Form1.cs
@@ -9,6 +9,7 @@ using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.IO;
 using System.Xml;
+using System.Xml.Linq;
 
 namespace codesnippet_generator
 {
@@ -46,9 +47,9 @@ namespace codesnippet_generator
             escritor.WriteStartElement("CodeSnippet");
             escritor.WriteAttributeString("Format", "1.0.0");
             escritor.WriteStartElement("Header");
-                    escritor.WriteElementString("Title", txt_Titulo.Text.ToString());
-                    escritor.WriteElementString("Shortcut", Txt_accesodirecto.Text.ToString());
-                    escritor.WriteElementString("Description", Txt_comentario.Text.ToString());
+                    escritor.WriteElementString("Title", txt_Titulo.Text.ToString().Trim());
+                    escritor.WriteElementString("Shortcut", Txt_accesodirecto.Text.ToString().Trim());
+                    escritor.WriteElementString("Description", Txt_comentario.Text.ToString().Trim());
                     escritor.WriteElementString("Author", "Gonzalo Santiago Martinez");
                             escritor.WriteStartElement("SnippetTypes");
                                 escritor.WriteElementString("SnippetType", "Expansion");
@@ -62,9 +63,9 @@ namespace codesnippet_generator
                 for (int i = 0; i < id.Length; i++)
                 {
                     escritor.WriteStartElement("Literal");
-                    escritor.WriteElementString("ID", id[i].ToString());
-                    escritor.WriteElementString("ToolTip", tooltip[i].ToString());
-                    escritor.WriteElementString("Default", valdefecto[i].ToString());
+                    escritor.WriteElementString("ID", id[i].ToString().Trim());
+                    escritor.WriteElementString("ToolTip", tooltip[i].ToString().Trim());
+                    escritor.WriteElementString("Default", valdefecto[i].ToString().Trim());
                     escritor.WriteEndElement();
 
                 }
@@ -72,9 +73,17 @@ namespace codesnippet_generator
 
 
                 escritor.WriteStartElement("Code");
-                escritor.WriteAttributeString("Language", cmb_lenguajes.Text);
+                escritor.WriteAttributeString("Language", cmb_lenguajes.Text.Trim());
 
-                escritor.WriteCData(txt_Codigo.Text+"$end$");
+                if (check_final.Checked)
+                {
+                    escritor.WriteCData(txt_Codigo.Text.Trim() + "$end$");
+                }
+                else
+                {
+                    escritor.WriteCData(txt_Codigo.Text.Trim());
+                }
+            
 
                 escritor.WriteEndElement();
 
@@ -82,7 +91,7 @@ namespace codesnippet_generator
                     for (int i = 0; i < usings.Length; i++)
                     {
                         escritor.WriteStartElement("Import");
-                        escritor.WriteElementString("Namespace", usings[i].ToString());
+                        escritor.WriteElementString("Namespace", usings[i].ToString().Trim());
                         escritor.WriteEndElement();
                     }
             escritor.WriteEndElement();
@@ -100,6 +109,196 @@ namespace codesnippet_generator
         private void Form1_Load(object sender, EventArgs e)
         {
             cmb_lenguajes.SelectedIndex = 1;
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog1 = new OpenFileDialog
+            {//buscar archivo
+              
+                InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.Desktop),
+                Title = "Buscar Snippets",
+
+                CheckFileExists = true,
+                CheckPathExists = true,
+
+                DefaultExt = "snippet",
+                Filter = "Archivos Snippet (*.snippet)|*.snippet",
+                FilterIndex = 2,
+                RestoreDirectory = true,
+
+                ReadOnlyChecked = true,
+                ShowReadOnly = true
+            };
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                txt_direccion.Text = openFileDialog1.FileName;
+
+                abrirSnippet();
+            }
+        }
+
+        private void abrirSnippet()
+        {
+            txt_Codigo.Text = "";
+            Txt_comentario.Text = "";
+            Txt_descripcion.Text = "";
+            txt_ID.Text = "";
+            Txt_predeterminado.Text = "";
+            txt_Titulo.Text = "";
+            txt_usings.Text = "";
+
+            XmlDocument xdoc = new XmlDocument();
+            xdoc.Load(@txt_direccion.Text);
+
+            try
+            {
+                txt_Titulo.Text = xdoc.GetElementsByTagName("Title")[0].InnerText;
+            }
+            catch (Exception)
+            {
+
+            }
+
+            try
+            {
+                Txt_accesodirecto.Text = xdoc.GetElementsByTagName("Shortcut")[0].InnerText;
+            }
+            catch (Exception)
+            {
+
+            }
+
+            try
+            {
+                Txt_comentario.Text = xdoc.GetElementsByTagName("Description")[0].InnerText;
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+            try
+            {
+                XElement XTemp = XElement.Load(@txt_direccion.Text);
+                var queryCDATAXML = from element in XTemp.DescendantNodes()
+                                    where element.NodeType == System.Xml.XmlNodeType.CDATA
+                                    select element.Parent.Value.Trim();
+                txt_Codigo.Text = queryCDATAXML.ToList<string>()[0].ToString();
+            }
+            catch (Exception)
+            {
+
+            }
+
+
+            try
+            {
+                if (txt_Codigo.Text.EndsWith("$end$"))
+                {
+                    check_final.Checked = true;
+                    try
+                    {
+                        txt_Codigo.Text = txt_Codigo.Text.Substring(0, txt_Codigo.Text.Length - 5);
+                    }
+                    catch (Exception)
+                    {
+
+                    }
+                }
+                else
+                {
+                    check_final.Checked = false;
+                }
+            }
+            catch (Exception)
+            {
+            }
+            
+
+            try
+            {
+                var res = xdoc.GetElementsByTagName("Code");
+                cmb_lenguajes.Text = res[0].Attributes["Language"].Value;
+            }
+            catch (Exception)
+            {
+
+            }
+            
+
+
+            var ids = xdoc.GetElementsByTagName("ID");
+            foreach (XmlNode item in ids)
+            {
+                txt_ID.Text += item.InnerText + ", ";
+            }
+
+            var descps = xdoc.GetElementsByTagName("ToolTip");
+            foreach (XmlNode item in descps)
+            {
+                Txt_descripcion.Text += item.InnerText + ", ";
+            }
+
+            var defaultz = xdoc.GetElementsByTagName("Default");
+            foreach (XmlNode item in defaultz)
+            {
+                Txt_predeterminado.Text += item.InnerText + ", ";
+            }
+
+            var usninzsd = xdoc.GetElementsByTagName("Namespace");
+            foreach (XmlNode item in usninzsd)
+            {
+                txt_usings.Text += item.InnerText + ", ";
+            }
+
+            //txt_ID.Text = xdoc.GetElementsByTagName("ID")[0].InnerText;
+            //Txt_descripcion.Text = xdoc.GetElementsByTagName("ToolTip")[0].InnerText;
+            //Txt_predeterminado.Text = xdoc.GetElementsByTagName("Default")[0].InnerText;
+            //txt_usings.Text = xdoc.GetElementsByTagName("Namespace")[0].InnerText;
+            if (txt_ID.Text.EndsWith(" "))
+            {
+                try
+                {
+                    txt_ID.Text = txt_ID.Text.Substring(0, txt_ID.Text.Length - 2);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (Txt_descripcion.Text.EndsWith(" "))
+            {
+                try
+                {
+                    Txt_descripcion.Text = Txt_descripcion.Text.Substring(0, Txt_descripcion.Text.Length - 2);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (Txt_predeterminado.Text.EndsWith(" "))
+            {
+                try
+                {
+                    Txt_predeterminado.Text = Txt_predeterminado.Text.Substring(0, Txt_predeterminado.Text.Length - 2);
+                }
+                catch (Exception)
+                {
+                }
+            }
+            if (txt_usings.Text.EndsWith(" "))
+            {
+                try
+                {
+                    txt_usings.Text = txt_usings.Text.Substring(0, txt_usings.Text.Length - 2);
+                }
+                catch (Exception)
+                {  
+                }
+            }
+
         }
     }
 }
